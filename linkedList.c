a1c716a0e329d955b2614948f702ae1594fcc1f2
Andrew <andriymartunov131@gmail.com>
2024-11-21 18:40:40 +0200

    Initial commit: Add linkedList.c

---
+++ linkedList.c
@@ -0,0 +1,193 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct Node
+{
+    int number;
+    struct Node *next;
+};
+
+int countNodes(struct Node *head) // Counts how many nodes in list (start from the first node)
+{
+    int counter = 0;
+
+    while (head != NULL)
+    {
+        printf("Node:%d  Value:%d\n", counter, head->number);
+        counter++;
+        head = head->next;
+    }
+
+    return counter;
+}
+
+struct Node *createNode(int numb) // Creates a new Node
+{
+    struct Node *newNode = malloc(sizeof(struct Node));
+
+    newNode->number = numb;
+    newNode->next = NULL;
+
+    return newNode;
+}
+
+void findNodeValue(struct Node *head, int value) // Finds node velue (start from the first node)
+{
+    while (head != NULL)
+    {
+        if (head->number == value)
+        {
+            printf("Number was found! (%d)\n", head->number);
+            return;
+        }
+        head = head->next;
+    }
+
+    printf("Number was not found!\n");
+}
+
+struct Node *insertNewNodeAtBeginning(struct Node *head, int value) // Inserts a new node at the beginning of list
+{
+    struct Node *newNode = createNode(value);
+
+    newNode->next = head;
+
+    head = newNode;
+
+    return head;
+}
+
+struct Node *insertNewNodeAtTheEnd(struct Node *head, int value) // Inserts a new node at the end of list
+{
+    struct Node *newNode = createNode(value);
+
+    if (head == NULL)
+    {
+        return newNode;
+    }
+
+    while (head->next != NULL)
+    {
+        head = head->next;
+    }
+
+    head->next = newNode;
+
+    return head;
+}
+
+struct Node *insertNewNode(struct Node *head, int targetValue, int newValue, bool insertAtTheBeginning) // Inserts a new node at the specified location 
+{
+    struct Node *newNode = createNode(newValue);
+
+    struct Node *temp;
+
+    if (insertAtTheBeginning)
+    {
+        newNode->next = head;
+
+        head = newNode;
+
+        return head;
+    }
+
+    while (head->number != targetValue)
+    {
+        head = head->next;
+
+        if (head->next == NULL)
+        {
+            printf("Next is NULL\n");
+            break;
+        }
+        
+    }
+
+    temp = head->next;
+
+    head->next = newNode;
+
+    newNode->next = temp;
+
+    return head;
+}
+
+struct Node *deleteNode(struct Node *head, int targetValue, bool isAtTheBeginning) // Deletes a node at the specified location
+{
+    struct Node *deleteNode = NULL;
+
+    struct Node *temp;
+
+    if(isAtTheBeginning)
+    {
+        temp = head->next;
+
+        head = deleteNode;
+
+        head = temp;
+        
+        return head;
+    }
+
+    while (head != NULL)
+    {
+        if (head->next == NULL)
+        {
+            return head;
+        }  
+        if (head->next->number == targetValue)
+        {
+            break;
+        }
+        
+        head = head->next;
+    }
+
+    if (head->next->next != NULL)
+    {
+        temp = head->next->next;
+        head->next = temp;
+    }
+    else
+    {
+        head->next = deleteNode;
+    }
+
+    return head;
+}
+
+void freeList(struct Node *head) {  
+    struct Node *temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
+int main()
+{
+    struct Node *head = createNode(10);
+    struct Node *second = createNode(20);
+    struct Node *third = createNode(30);
+    struct Node *fourth = createNode(40);
+
+    head->next = second;
+    second->next = third;
+    third->next = fourth;
+
+    countNodes(head);
+
+    printf("\n");
+
+    insertNewNode(head, 30, 45, false);
+
+    head = deleteNode(head, 10, true);
+
+    findNodeValue(head, 45);
+
+    countNodes(head);
+
+    return 0;
+}
