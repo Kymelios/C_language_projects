d0183a20a241fc896a36cdb329a8a05e263983f5
Andrew <andriymartunov131@gmail.com>
2024-12-23 16:12:56 +0200

    Update binary_tree_ver2.c
    Added:
    1. Comments to functions.
    2. Function to calculate amount of nodes in the tree.
    3. Function to find a searching value in the tree.
    4. Function to calculate the tree's height.
    Updated:
    1. Function preorderPrint was changed output for better reading.

--- Trees implementation/binary_tree_ver2.c
+++ Trees implementation/binary_tree_ver2.c
@@ -8,6 +8,7 @@ struct node
     struct node *right;
 };
 
+//This function creates a new node and allocate memory for it
 struct node *createNode(int value)
 {
     struct node *newNode = malloc(sizeof(struct node));
@@ -18,17 +19,48 @@ struct node *createNode(int value)
     return newNode;
 }
 
+//This functions prints tree in preorder traversal
 void preorderPrint(struct node *root)
 {
     if (root == NULL)
     {
         return;
     }
-    printf("%d", root->value);
+    printf("-%d-", root->value);
     preorderPrint(root->left);
     preorderPrint(root->right);
 }
 
+//This function calculates a height of the tree
+int treeHeight(struct node* root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+
+    int leftHeight = treeHeight(root->left);
+    int rightHeight = treeHeight(root->right);
+    
+    int result = ((leftHeight > rightHeight) ? leftHeight : rightHeight) + 1;
+    return result;
+}
+
+//This function calculates real amount of nodes in tree
+int amountOfNodes(struct  node* root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+
+    int leftNodes = amountOfNodes(root->left);
+    int rightNodes = amountOfNodes(root->right);
+
+    return (leftNodes + rightNodes) + 1;
+}
+
+//This function finds a needed value in the tree
 int findValue(struct node *root, int targetValue)
 {
     if (root == NULL)
@@ -54,6 +86,9 @@ int main()
 
     printf("Was value found? (0 is false, 1 is true) \n--->(%d)\n\n", findValue(root, 1));
     printf("Was value found? (0 is false, 1 is true) \n--->(%d)\n\n", findValue(root, 32));
+
+    printf("Tree height: %d\n", treeHeight(root));
+    printf("Amount of nodes: %d\n", amountOfNodes(root));
 
     printf("Preorder: ");
     preorderPrint(root);
